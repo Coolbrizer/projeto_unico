52acbb265aa4b6345e084b07f58f0b5bc1306caa
coolbrizer <alexandre.cdamasceno@gmail.com>
2026-07-28 15:09:01 -0300

    Update migration_normalizar_responsavel_atividades.sql

--- supabase/migration_normalizar_responsavel_atividades.sql
+++ supabase/migration_normalizar_responsavel_atividades.sql
@@ -1,13 +1,19 @@
--- Normaliza public.atividades.responsavel removendo o prefixo de matrícula.
+-- Normaliza campos de nomes removendo o prefixo de matrícula.
 --
 -- Antes:
 --   5030 | SANDRA VERONICA ALVES RODRIGUES
+--   10274 ODILAIR DE OLIVEIRA LEÃO
 --
 -- Depois:
 --   SANDRA VERONICA ALVES RODRIGUES
+--   ODILAIR DE OLIVEIRA LEÃO
 --
--- O script altera somente valores que começam com:
+-- Em public.atividades.responsavel, altera somente valores que começam com:
 --   número + pipe + nome
+--
+-- Em public.equipe.equipe, altera valores que começam com:
+--   número + pipe + nome
+--   número + nome
 --
 -- Também aceita variações visuais do pipe: |, │ e ｜.
 --
@@ -17,7 +23,7 @@
 
 BEGIN;
 
--- Prévia das linhas que serão alteradas.
+-- Prévia das linhas que serão alteradas em atividades.responsavel.
 SELECT
   id,
   codigo,
@@ -30,6 +36,19 @@ FROM public.atividades
 WHERE responsavel ~ '^\s*[0-9]+\s*[|│｜]\s*'
 ORDER BY codigo;
 
+-- Prévia das linhas que serão alteradas em equipe.equipe.
+SELECT
+  id,
+  codigo,
+  equipe AS equipe_atual,
+  NULLIF(
+    btrim(regexp_replace(equipe, '^\s*[0-9]+\s*([|│｜]\s*)?', '')),
+    ''
+  ) AS equipe_normalizada
+FROM public.equipe
+WHERE equipe ~ '^\s*[0-9]+(\s*[|│｜])?\s+'
+ORDER BY codigo;
+
 UPDATE public.atividades
 SET responsavel = NULLIF(
   btrim(regexp_replace(responsavel, '^\s*[0-9]+\s*[|│｜]\s*', '')),
@@ -37,9 +56,21 @@ SET responsavel = NULLIF(
 )
 WHERE responsavel ~ '^\s*[0-9]+\s*[|│｜]\s*';
 
+UPDATE public.equipe
+SET equipe = NULLIF(
+  btrim(regexp_replace(equipe, '^\s*[0-9]+\s*([|│｜]\s*)?', '')),
+  ''
+)
+WHERE equipe ~ '^\s*[0-9]+(\s*[|│｜])?\s+';
+
 -- Conferência: deve retornar 0 após a atualização.
 SELECT count(*) AS responsaveis_ainda_com_matricula_pipe
 FROM public.atividades
 WHERE responsavel ~ '^\s*[0-9]+\s*[|│｜]\s*';
+
+-- Conferência: deve retornar 0 após a atualização.
+SELECT count(*) AS equipes_ainda_com_matricula
+FROM public.equipe
+WHERE equipe ~ '^\s*[0-9]+(\s*[|│｜])?\s+';
 
 COMMIT;
