86265c7d3ee0e95636eb9244f71cb6b4b79175f6
coolbrizer <alexandre.cdamasceno@gmail.com>
2026-07-28 14:51:51 -0300

    Update migration_normalizar_responsavel_atividades.sql

--- supabase/migration_normalizar_responsavel_atividades.sql
+++ supabase/migration_normalizar_responsavel_atividades.sql
@@ -10,6 +10,10 @@
 --   número + pipe + nome
 --
 -- Também aceita variações visuais do pipe: |, │ e ｜.
+--
+-- Atenção: commit/deploy da aplicação não executa este SQL no banco.
+-- Execute este arquivo no SQL Editor do Supabase, ou aplique via ferramenta
+-- de migração do banco.
 
 BEGIN;
 
@@ -19,23 +23,23 @@ SELECT
   codigo,
   responsavel AS responsavel_atual,
   NULLIF(
-    btrim(regexp_replace(responsavel, '^\s*\d+\s*[|│｜]\s*', '')),
+    btrim(regexp_replace(responsavel, '^\s*[0-9]+\s*[|│｜]\s*', '')),
     ''
   ) AS responsavel_normalizado
 FROM public.atividades
-WHERE responsavel ~ '^\s*\d+\s*[|│｜]\s*'
+WHERE responsavel ~ '^\s*[0-9]+\s*[|│｜]\s*'
 ORDER BY codigo;
 
 UPDATE public.atividades
 SET responsavel = NULLIF(
-  btrim(regexp_replace(responsavel, '^\s*\d+\s*[|│｜]\s*', '')),
+  btrim(regexp_replace(responsavel, '^\s*[0-9]+\s*[|│｜]\s*', '')),
   ''
 )
-WHERE responsavel ~ '^\s*\d+\s*[|│｜]\s*';
+WHERE responsavel ~ '^\s*[0-9]+\s*[|│｜]\s*';
 
 -- Conferência: deve retornar 0 após a atualização.
 SELECT count(*) AS responsaveis_ainda_com_matricula_pipe
 FROM public.atividades
-WHERE responsavel ~ '^\s*\d+\s*[|│｜]\s*';
+WHERE responsavel ~ '^\s*[0-9]+\s*[|│｜]\s*';
 
 COMMIT;
